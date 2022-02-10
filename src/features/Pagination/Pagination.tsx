cf24d6b67cec13f65bbad1d4bad56c53a8468fa4
dotSality <90727519+dotSality@users.noreply.github.com>
2022-02-11 00:53:08 +0300

    fixed pagination logic

--- src/features/Pagination/Pagination.tsx
+++ src/features/Pagination/Pagination.tsx
@@ -14,7 +14,7 @@ export const Pagination = ({ title, type }: PropsType) => {
   const dispatch = useAppDispatch();
   const { totalResults, page } = useAppSelector(state => state.movies);
   const moviesOnPage = 10;
-  const pages: number[] = [];
+  let pages: number[] = [];
   if (totalResults && page) {
     const pagesCount = Math.ceil(totalResults / moviesOnPage);
     const currentPage = page;
@@ -22,12 +22,16 @@ export const Pagination = ({ title, type }: PropsType) => {
       for (let i = 1; i <= pagesCount; i += 1) {
         pages.push(i);
       }
-    } else if (currentPage > 6 && currentPage < pagesCount - 6) {
+    } else if (currentPage >= 6 && currentPage < pagesCount - 6) {
       for (let i = currentPage - 4; i <= currentPage + 4; i += 1) {
         pages.push(i);
       }
+    } else if (currentPage <= 6) {
+      pages = Array(9)
+        .fill(0)
+        .map((_, i) => i + 1);
     } else {
-      for (let i = currentPage; i < currentPage + 9; i += 1) {
+      for (let i = pagesCount - 8; i < pagesCount; i += 1) {
         pages.push(i);
       }
     }
